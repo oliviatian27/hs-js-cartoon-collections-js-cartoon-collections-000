b90350d08bc2fa7b35d83b8703fa38eab00ab394
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-07-10 19:44:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 function dwarfRollCall(dwarves) {
-var result= dwarves.map(function(value,index){
-   return (index+1).toString()+'. '+value+' ';
-  });
- return result.join('');
- }
-
+   var result= dwarves.map(function(value,index){
+      return (index+1).toString()+'. '+value+' ';
+       });
+   return result.join('');
+  }
 
 function summonCaptainPlanet(planeteerCalls){
- return planeteerCalls.map(x=>
+  return planeteerCalls.map(x=>
  x.toUpperCase()+'!');
-}
+ }
 
 function longPlaneteerCalls(words) {
   return words.some(x=>x.length>4 );
@@ -18,8 +17,8 @@ function longPlaneteerCalls(words) {
 function findTheCheese(food){
   var cheese=['cheddar','gouda','camembert'];
   var result=food.filter(x=>cheese.indexOf(x)!=-1);
- if (result.length>0){
+  if (result.length>0){
    return result[0];
- }
+    }
   return "no cheese!";
-}
+ }
